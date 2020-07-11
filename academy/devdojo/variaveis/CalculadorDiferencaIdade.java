ed3e0e2c53a159279d1d500871346d593a65b081
Enzo Alexsander <enzoalexsander9@gmail.com>
2020-07-11 19:11:21 -0300

    Variaveis

---
+++ academy/devdojo/variaveis/CalculadorDiferencaIdade.java
@@ -0,0 +1,10 @@
+package academy.devdojo.variaveis;
+
+public class CalculadorDiferencaIdade {
+    public static void main(String[] args){
+        int firstAge = 74;
+        int secondAge = 39;
+        int ageDifference = firstAge - secondAge;
+        System.out.println("A diferenca entre idades eh: " + ageDifference);
+    }
+}
