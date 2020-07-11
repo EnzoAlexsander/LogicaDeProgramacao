4f965a4d080a26b259cb0df71362ba1a66a66c70
Enzo Alexsander <enzoalexsander9@gmail.com>
2020-07-11 19:11:58 -0300

    Variaveis

---
+++ academy/devdojo/variaveis/TestandoVariaveis.java
@@ -0,0 +1,21 @@
+package academy.devdojo.variaveis;
+/*
+char = Quando é necessário utilizar apenas uma caractere
+String = Quando é necessário utilizar mais de um caractere
+boolean = No java, obrigatoriamente escrever false ou True.
+ */
+
+public class TestandoVariaveis {
+    public static void main(String[] args) {
+        char masculino = 'A';
+        char feminino = 'a';
+        System.out.println(""+masculino+feminino);
+
+        String nome = "Aqui você pode colocar frases bem grandes!";
+        System.out.println(nome);
+
+        boolean condicao = false;
+        System.out.println(condicao);
+
+    }
+}
