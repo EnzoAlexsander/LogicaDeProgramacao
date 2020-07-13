004cb3b677a9f10655474665d251e930415827df
Enzo Alexsander <enzoalexsander9@gmail.com>
2020-07-13 16:58:08 -0300

    Laços de Repetição

---
+++ academy/devdojo/lacosderepeticao/fors/IntroducaoFors.java
@@ -0,0 +1,17 @@
+package academy.devdojo.lacosderepeticao.fors;
+
+public class IntroducaoFors {
+    public static void main(String[] args) {
+        int contador;
+        for(contador = 0;contador <= 10;contador++) {
+            System.out.println(contador);
+        }
+        /*
+        outra forma
+        for (int contador = 0;contador <= 10; contador++) {
+            System.out.println(contador);
+        }
+         */
+        System.out.println(contador);
+    }
+}
