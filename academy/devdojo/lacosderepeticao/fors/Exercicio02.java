d12b6492100ce30430231cba0fccbffd5a942573
Enzo Alexsander <enzoalexsander9@gmail.com>
2020-07-13 16:57:32 -0300

    Laços de Repetição

---
+++ academy/devdojo/lacosderepeticao/fors/Exercicio02.java
@@ -0,0 +1,12 @@
+package academy.devdojo.lacosderepeticao.fors;
+/*
+Eu como usuario gostaria de um sistema que
+fizesse automaticamente a tabuada de 1 a 10
+ */
+public class Exercicio02 {
+    public static void main(String[] args) {
+        for (int i = 1; i <=10 ; i++) {
+            System.out.println(5*i);
+        }
+    }
+}
