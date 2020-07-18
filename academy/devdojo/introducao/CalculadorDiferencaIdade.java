f34f2e67ecebe6b22620aaf3ad1ddd81607ebc63
Enzo Alexsander <enzoalexsander9@gmail.com>
2020-07-18 19:10:07 -0300

    Introdução

---
+++ academy/devdojo/introducao/CalculadorDiferencaIdade.java
@@ -0,0 +1,15 @@
+/*
+Criar uma classe chamada CalculadorDiferencaIdade
+Crie uma variável para guardar a primeira idade
+Crie uma variável para guardar a segunda idade
+Crie uma variável para guardar a diferença entre a idade 1 e a idade 2
+Imprima a diferença no console
+*/
+public class CalculadorDiferencaIdade {
+	public static void main(String[] args){
+		int primeiraIdade = 74;
+		int segundaIdade = 39;
+		int diferencaIdade = primeiraIdade - segundaIdade;
+		System.out.println("A diferenca entre idades eh: " + diferencaIdade);
+	}
+}
