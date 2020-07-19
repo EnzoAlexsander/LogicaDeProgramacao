e969cc8b0c2a993132daeacb19cb7f68ab16727e
Enzo Alexsander <enzoalexsander9@gmail.com>
2020-07-19 12:56:56 -0300

    Introdução

---
+++ academy/devdojo/introducao/ViradoNoJiraya3.java
@@ -0,0 +1,12 @@
+//Tudo o que estiver nesta linha será ignorado
+/*
+Tudo o que estiver 
+Entre essas linhas
+será ignorado.
+*/
+public class ViradoNoJiraya3 {
+	public static void main (String[] args){
+		int idade = 45;
+		System.out.println(idade + " eh a idade dele");
+	}
+}
